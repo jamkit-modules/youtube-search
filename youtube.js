e1437e779dea9b3378921e0ee8d2e4eeb5221eb3
Hanyeol Cho <hanyeol.cho@gmail.com>
2022-12-17 17:13:02 +0900

    Update for YouTube's tag modification

--- youtube.js
+++ youtube.js
@@ -3,9 +3,9 @@ function getVideos(nextToken, onResult, onError) {
         if (_loadNextPage()) {
             setTimeout(function() {
                 _getVideos(nextToken, 1, onResult, onError);
-            }, 200)    
+            }, 200);
         } else {
-            onResult({ "videos":[] })
+            onResult({ "videos":[] });
         }
     } else {
         _getVideos(0, 0, onResult, onError);        
@@ -14,18 +14,18 @@ function getVideos(nextToken, onResult, onError) {
 
 function _getVideos(location, waitingCount, onResult, onError) {
     try {
-        var videoItems = document.getElementsByTagName('ytm-compact-video-renderer')
-        var videos = []
+        var videoItems = document.getElementsByTagName('ytm-video-with-context-renderer');
+        var videos = [];
 
         if (videoItems.length > location) {
             for (var i = location; i < videoItems.length; i++) {
                 try {
                     videos.push({
-                        "url":videoItems[i].getElementsByClassName('compact-media-item-image')[0].href,
-                        "title":videoItems[i].getElementsByClassName('compact-media-item-headline')[0].textContent,
-                        "viewCount":videoItems[i].getElementsByClassName('compact-media-item-stats')[0].textContent,
-                        "publishedDate":videoItems[i].getElementsByClassName('compact-media-item-stats')[1].textContent
-                    })
+                        "url": videoItems[i].getElementsByClassName('media-item-thumbnail-container')[0].href,
+                        "title": videoItems[i].getElementsByClassName('media-item-headline')[0].textContent,
+                        "viewCount": videoItems[i].getElementsByClassName('ytm-badge-and-byline-item-byline')[1].textContent,
+                        "publishedDate": videoItems[i].getElementsByClassName('ytm-badge-and-byline-item-byline')[2].textContent
+                    });
                 } catch (e) {
                     // eat up
                 }
@@ -33,15 +33,15 @@ function _getVideos(location, waitingCount, onResult, onError) {
     
             if (waitingCount > 0) {
                 setTimeout(function() {
-                    _getVideos(location, waitingCount - 1, onResult, onError)
-                }, 200)
+                    _getVideos(location, waitingCount - 1, onResult, onError);
+                }, 200);
             } else {
-                onResult({ "videos":videos })
+                onResult({ "videos": videos });
             }
         } else {
             setTimeout(function() {
-                _getVideos(location, waitingCount, onResult, onError)
-            }, 200)
+                _getVideos(location, waitingCount, onResult, onError);
+            }, 200);
         }
     } catch (e) {
         onError();
